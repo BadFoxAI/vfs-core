96c085809aa9091a77c5a279282d6ec20b45e13e
BadFoxAI <157637164+BadFoxAI@users.noreply.github.com>
2026-02-23 18:56:44 +0000

    feat: flat root with compiled pkg folder

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use vfs_core::{Machine};
-
 fn main() {
-    let mut vm = Machine::new(vec![0x10, 10, 0, 0, 0, 0, 0, 0, 0, 0x10, 20, 0, 0, 0, 0, 0, 0, 0, 0x20, 0x00]);
-    while let Ok(true) = vm.step() {}
-    println!("CLI Result: {}", vm.stack.last().unwrap_or(&0));
+    vfs_core::main();
 }
